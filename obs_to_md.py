7a26c289470dc3102cfd37e1d3514f62e82e392f
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-07-19 11:01:40 -0700

    script update(use img)

--- obs_to_md.py
+++ obs_to_md.py
@@ -3,19 +3,18 @@ import re
 def convert_obsidian_to_md(input_line):
     # Recognize the first pattern and replace it
     r1 = re.compile(r'!\[\[(.*?\.\w+)\|(\d+?)\]\]')
-    line = r1.sub(r'![\1](\1){:width="\2px"}', input_line)
+    line = r1.sub(r'<img src="\1" width="\2">', input_line)
 
     # Recognize the second pattern and replace it
     r2 = re.compile(r'!\[\[(.*?\.\w+)\]\]')
-    line = r2.sub(r'![\1](\1)', line)
+    line = r2.sub(r'<img src="\1">', line)
 
     return line
 
-with open("Jam.md", "r") as file:
+with open("jam_obsidian.md", "r") as file:
     lines = file.readlines()
     
 conversion = [convert_obsidian_to_md(line) for line in lines]
 
-with open("Jam_md.md", "w") as output_file:
+with open("jam.md", "w") as output_file:
     output_file.writelines(conversion)
-
