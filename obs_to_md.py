759b690c9dc77485d38e34f401e374221b9128ff
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-07-20 13:03:01 -0700

    script update

--- obs_to_md.py
+++ obs_to_md.py
@@ -1,15 +1,18 @@
 import re
 
 def convert_obsidian_to_md(input_line):
+    base_url = "https://raw.githubusercontent.com/NalinPlad/GLSLFractals/main"
+    
     # Recognize the first pattern and replace it
     r1 = re.compile(r'!\[\[(.*?\.\w+)\|(\d+?)\]\]')
-    line = r1.sub(r'<img src="\1" width="\2">', input_line)
+    line = r1.sub(f'<img src="{base_url}/\\1" width="\\2">', input_line)
 
     # Recognize the second pattern and replace it
     r2 = re.compile(r'!\[\[(.*?\.\w+)\]\]')
-    line = r2.sub(r'<img src="\1">', line)
+    line = r2.sub(f'<img src="{base_url}/\\1">', line)
 
     return line
+
 
 with open("jam_obsidian.md", "r") as file:
     lines = file.readlines()
